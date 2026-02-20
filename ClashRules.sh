329371f936910054ddf990107b2eb5fa669b74ea
Martin Power <gorillapower@gmail.com>
2026-02-20 10:55:43 +0800

    added additional azure regions and updated rules

--- ClashRules.sh
+++ ClashRules.sh
@@ -3,37 +3,55 @@
 . /usr/share/openclash/log.sh
 . /lib/functions.sh
 
-LOG_OUT "Tip: Start Running Custom Overwrite Scripts..."
-LOGTIME=$(echo $(date "+%Y-%m-%d %H:%M:%S"))
+LOG_OUT "Configuring custom Clash rules..."
+LOGTIME=$(date "+%Y-%m-%d %H:%M:%S")
 LOG_FILE="/tmp/openclash.log"
 CONFIG_FILE="$1"
 
 ruby -ryaml -rYAML -I "/usr/share/openclash" -E UTF-8 -e "
   begin
-    Value = YAML.load_file('$CONFIG_FILE');
+    Value = YAML.load_file('$CONFIG_FILE')
     first_group = Value['proxy-groups'][0]
     
     if first_group && first_group['proxies']
       proxies = first_group['proxies']
 
-      # --- 1. PROXY GROUPS ---
+      Value['proxy-groups'].push({
+        'name' => 'Azure_West_Europe',
+        'type' => 'url-test',
+        'url' => 'https://s3westeurope.blob.core.windows.net/public/latency-test.json',
+        'interval' => 300,
+        'tolerance' => 50,
+        'lazy' => true,
+        'proxies' => proxies
+      })
       
-      # Azure West Europe
-      Value['proxy-groups'].push({'name' => 'Azure_West_Europe', 'type' => 'select', 'proxies' => proxies})
-      
-      # South Africa
+      # --- 1. AZURE US GROUPS ---
+      # US East (covers East US, East US 2)
+      Value['proxy-groups'].push({
+        'name' => 'Azure_US_East',
+        'type' => 'url-test',
+        'url' => 'http://s3eastus.blob.core.windows.net/public/latency-test.json', 
+        'interval' => 300,
+        'tolerance' => 50,
+        'proxies' => proxies
+      })
+
+      # US West (covers West US, West US 2, West US 3)
+      Value['proxy-groups'].push({
+        'name' => 'Azure_US_West',
+        'type' => 'url-test',
+        'url' => 'http://s3westus.blob.core.windows.net/public/latency-test.json', 
+        'interval' => 300,
+        'tolerance' => 50,
+        'proxies' => proxies
+      })
+
       Value['proxy-groups'].push({'name' => 'South Africa 🇿🇦', 'type' => 'select', 'proxies' => proxies})
-      
-      # Brazil
       Value['proxy-groups'].push({'name' => 'Brazil 🇧🇷', 'type' => 'select', 'proxies' => proxies})
-      
-      # CrunchyRoll
       Value['proxy-groups'].push({'name' => 'CrunchyRoll', 'type' => 'select', 'proxies' => proxies})
-      
-      # Australia
       Value['proxy-groups'].push({'name' => 'Aus 🇦🇺', 'type' => 'select', 'proxies' => proxies})
 
-      # --- 2. RULE PROVIDER (Azure) ---
       Value['rule-providers'] ||= {}
       Value['rule-providers']['Azure_West_Europe'] = {
         'type' => 'http',
@@ -43,12 +61,25 @@ ruby -ryaml -rYAML -I "/usr/share/openclash" -E UTF-8 -e "
         'interval' => 86400
       }
 
-      # --- 3. RULES (PRESERVED & NEW) ---
-      
-      # Azure Rule (Top Priority)
-      Value['rules'].unshift('RULE-SET,Azure_West_Europe,Azure_West_Europe')
+      Value['rule-providers']['Azure_US_East'] = {
+        'type' => 'http',
+        'behavior' => 'classical',
+        'url' => 'https://raw.githubusercontent.com/gorillapower/azure-ips-westeurope-clash/refs/heads/main/azure_us_east.yaml',
+        'path' => './rule_provider/azure_us_east.yaml',
+        'interval' => 86400
+      }
 
-      # South Africa Rules (Preserved)
+      Value['rule-providers']['Azure_US_West'] = {
+        'type' => 'http',
+        'behavior' => 'classical',
+        'url' => 'https://raw.githubusercontent.com/gorillapower/azure-ips-westeurope-clash/refs/heads/main/azure_west_east.yaml',
+        'path' => './rule_provider/azure_us_west.yaml',
+        'interval' => 86400
+      }
+      Value['rules'].unshift('RULE-SET,Azure_West_Europe,Azure_West_Europe')
+      Value['rules'].unshift('RULE-SET,Azure_US_East,Azure_US_East')
+      Value['rules'].unshift('RULE-SET,Azure_US_West,Azure_US_West')
+      
       sa_rules = [
         'IP-CIDR,66.22.96.0/24,South Africa 🇿🇦',
         'DOMAIN-SUFFIX,akamaized.net,South Africa 🇿🇦',
@@ -70,14 +101,12 @@ ruby -ryaml -rYAML -I "/usr/share/openclash" -E UTF-8 -e "
       ]
       sa_rules.reverse.each { |r| Value['rules'].unshift(r) }
 
-      # CrunchyRoll Rules (Preserved)
       cr_rules = [
         'DOMAIN-SUFFIX,crunchyroll.com,CrunchyRoll',
         'AND,((SRC-IP-CIDR,10.0.0.232/32),(DOMAIN,firebaseremoteconfigrealtime.googleapis.com)),CrunchyRoll'
       ]
       cr_rules.reverse.each { |r| Value['rules'].unshift(r) }
 
-      # Australia Rules (Preserved)
       au_rules = [
         'DOMAIN-SUFFIX,stan.com.au,Aus 🇦🇺',
         'DOMAIN-SUFFIX,stan.video,Aus 🇦🇺',
@@ -90,9 +119,10 @@ ruby -ryaml -rYAML -I "/usr/share/openclash" -E UTF-8 -e "
       
     end
     
-    File.open('$CONFIG_FILE','w') {|f| YAML.dump(Value, f)};
+    File.open('$CONFIG_FILE','w') {|f| YAML.dump(Value, f)}
+    puts '${LOGTIME} Clash rules configured successfully'
   rescue Exception => e
-    puts '${LOGTIME} Error: Custom Overwrite failed - ' + e.message;
+    puts '${LOGTIME} Error: ' + e.message
   end
 " 2>/dev/null >> $LOG_FILE
 
